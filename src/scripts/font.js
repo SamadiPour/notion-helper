224ac8531f11c28f18f8fb870bc0e8fc739baa31
SamadiPour <samadipoor2@gmail.com>
2021-09-30 22:18:04 +0330

    fix: Two parted fonts name not working

--- src/scripts/font.js
+++ src/scripts/font.js
@@ -19,28 +19,5 @@ function setFont(font) {
         }
     }
 
-    // ========================
-    addStyle(`* :not(.notion-code-block *) { font-family: ${font}, ui-sans-serif, -apple-system, system-ui, "Segoe UI", Helvetica, "Apple Color Emoji", Arial, sans-serif, "Segoe UI Emoji", "Segoe UI Symbol" !important; } `);
-
-    // ======================== works
-    // const elements = document.evaluate("//*[contains(@style,\'font-family\')]", document, null, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null);
-    // for (let i = 0; i < elements.snapshotLength; i++) {
-    //     elements.snapshotItem(i).style.fontFamily = font;
-    // }
-    // ========================
-    // const selectingQuery = 'h1,h2,h3,h4,h5,h6,p,li,td,tr,pre,font,blockquote,small,center,span,a,div,strong,input';
-    // const selected = document.querySelectorAll(selectingQuery);
-    // for (let i = 0; i < selected.length; i++) {
-    //     selected[i].style.fontFamily = `${font}, ui-sans-serif, -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, "Apple Color Emoji", Arial, sans-serif, "Segoe UI Emoji", "Segoe UI Symbol"`;
-    // }
-    // ========================
-    // const selectingQuery = 'h1,h2,h3,h4,h5,h6,p,li,td,tr,pre,font,blockquote,small,center,span,a,div,strong,input';
-    // const selected = new Set([
-    //     ...document.getElementsByClassName('notion-frame')[0].querySelectorAll(selectingQuery),
-    //     ...document.getElementsByClassName('notion-topbar')[0].querySelectorAll(selectingQuery)
-    // ]);
-    // for (let item in selected) {
-    //     item.style.fontFamily = `${font}, ui-sans-serif, -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, "Apple Color Emoji", Arial, sans-serif, "Segoe UI Emoji", "Segoe UI Symbol"`;
-    // }
-    // ========================
+    addStyle(`* :not(.notion-code-block *) { font-family: ${font ? '"' + font + '",' : ''} ui-sans-serif, -apple-system, system-ui, "Segoe UI", Helvetica, "Apple Color Emoji", Arial, sans-serif, "Segoe UI Emoji", "Segoe UI Symbol" !important; } `);
 }
